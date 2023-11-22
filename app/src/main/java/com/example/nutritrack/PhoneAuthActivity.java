cd52a79086142e1e48e15d05e1286c594546fdef
DavidOngora <ongoradavid5.gmail.com>
2023-11-22 10:26:34 +0300

    feat:method for phone authenicatio

--- app/src/main/java/com/example/nutritrack/PhoneAuthActivity.java
+++ app/src/main/java/com/example/nutritrack/PhoneAuthActivity.java
@@ -1,2 +1,128 @@
-package com.example.nutritrack;public class PhoneAuthActivity {
+package com.example.nutritrack;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class PhoneAuthActivity extends AppCompatActivity {
+
+    private EditText phoneNumberEditText;
+    private EditText codeEditText;
+    private Button sendCodeButton;
+    private Button verifyCodeButton;
+
+    private FirebaseAuth auth;
+    private String verificationId;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone);
+
+        phoneNumberEditText = findViewById(R.id.number);
+        codeEditText = findViewById(R.id.code);
+        sendCodeButton = findViewById(R.id.sendCode);
+        verifyCodeButton = findViewById(R.id.phoneLoginButton);
+
+        auth = FirebaseAuth.getInstance();
+
+        sendCodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phoneNumber = phoneNumberEditText.getText().toString().trim();
+
+                if (TextUtils.isEmpty(phoneNumber)) {
+                    Toast.makeText(PhoneAuthActivity.this, "Please enter a phone number", Toast.LENGTH_SHORT).show();
+                } else {
+                    sendVerificationCode(phoneNumber);
+                }
+            }
+        });
+
+        verifyCodeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String code = codeEditText.getText().toString().trim();
+
+                if (TextUtils.isEmpty(code)) {
+                    Toast.makeText(PhoneAuthActivity.this, "Please enter the verification code", Toast.LENGTH_SHORT).show();
+                } else {
+                    verifyCode(code);
+                }
+            }
+        });
+    }
+
+    private void sendVerificationCode(String phoneNumber) {
+        PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                phoneNumber,
+                60,
+                TimeUnit.SECONDS,
+                this,
+                new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+                    @Override
+                    public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                        signInWithPhoneAuthCredential(phoneAuthCredential);
+                    }
+
+                    @Override
+                    public void onVerificationFailed(@NonNull FirebaseException e) {
+                        Toast.makeText(PhoneAuthActivity.this, "Verification failed: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onCodeSent(@NonNull String s, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                        super.onCodeSent(s, forceResendingToken);
+                        verificationId = s;
+                        // Show UI for entering the code
+                        // You can show an EditText for entering the code here
+                    }
+                }
+        );
+    }
+
+    private void verifyCode(String code) {
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationId, code);
+        signInWithPhoneAuthCredential(credential);
+    }
+
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+        auth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Toast.makeText(PhoneAuthActivity.this, "Verification successful", Toast.LENGTH_SHORT).show();
+                            // Navigate to the desired activity
+                            // For example:
+                            Intent intent = new Intent(PhoneAuthActivity.this, LogOutActivity.class);
+                            startActivity(intent);
+                        } else {
+                            // Sign in failed, display a message and update the UI
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                // The verification code entered was invalid
+                                Toast.makeText(PhoneAuthActivity.this, "Invalid verification code", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+    }
 }
